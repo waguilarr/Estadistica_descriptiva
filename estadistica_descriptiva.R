212582e97b7c724e271ccca45aaf76fd527a4f4c
William Aguilar <william.aguilar@simm.com.co>
2021-05-23 16:02:59 -0500

    Reemplazar los campos vacios de la columna antiguedad máxima por la media general y un boxplot para el análisis

--- estadistica_descriptiva.R
+++ estadistica_descriptiva.R
@@ -1,2 +1,15 @@
+library(dplyr)
+library(ggplot2)
+
 siniestros <- read.csv(
   "C:/Users/William/Downloads/Data_Siniestros en Seguros.csv")
+
+#Antiguedad máxima
+media_antiguedad <- mean(siniestros[!is.na(siniestros$Antigüedad_Maxima),]$Antigüedad_Maxima)
+siniestros[is.na(siniestros$Antigüedad_Maxima),]$Antigüedad_Maxima <- media_antiguedad
+
+ggplot(data = siniestros, mapping = aes(y=Antigüedad_Maxima)) +
+  geom_boxplot(fill = "#F34213", color="#2E2E3A") + 
+  scale_x_discrete("") +
+  ylab("Antiguedad máxima") +
+  theme_gray()
